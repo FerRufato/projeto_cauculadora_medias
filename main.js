c8c9f8cbdf73528b219f7e8d19319c20eab44a79
Fernanda Rufato <nandaoliveira1985@terra.com.br>
2024-01-20 00:26:29 -0300

    fix: limitação de casas decimais

--- main.js
+++ main.js
@@ -51,7 +51,7 @@ CorpoTabela.innerHTML = linhas;
 function atualizaMediaFinal() {
     const mediaFinal = cauculaMediaFinal();
 
-    document.getElementById('media-final-valor').innerHTML = mediaFinal;
+    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2);
     document.getElementById('media-final-resultado').innerHTML = mediaFinal >=notaMinima ? spanAprovado : spanReprovado;
 
 }
